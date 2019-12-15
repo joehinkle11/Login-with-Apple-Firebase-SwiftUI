83939d0a780d67a06d93cedca8ebc75fad09add4
Joe Hinkle <joehinkle11@gmail.com>
2019-12-15 14:33:30 -0500

    button base made with some firebase stuff

--- LoginWithAppleFirebaseSwiftUI/ContentView.swift
+++ LoginWithAppleFirebaseSwiftUI/ContentView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("under development...")
+        NavigationView {
+            VStack {
+                SignInWithAppleToFirebase()
+                    .frame(height: 50, alignment: .center)
+                    .padding(25)
+                Button(action: {
+                    let webURL = URL(string: "https://github.com/joehinkle11/Login-with-Apple-Firebase-SwiftUI")!
+                    if UIApplication.shared.canOpenURL(webURL as URL) {
+                        UIApplication.shared.open(webURL)
+                    }
+                }) {
+                    Text("Source Code on GitHub 📃")
+                }.padding([.bottom], 5)
+                Button(action: {
+                    let screenName =  "joehink95"
+                    let appURL = URL(string: "twitter://user?screen_name=\(screenName)")!
+                    let webURL = URL(string: "https://twitter.com/\(screenName)")!
+                    if UIApplication.shared.canOpenURL(appURL as URL) {
+                        UIApplication.shared.open(appURL)
+                    } else {
+                        UIApplication.shared.open(webURL)
+                    }
+                }) {
+                    Text("Contact me ♥️")
+                }.padding([.bottom], 5)
+                Button(action: {
+                    let tweetText = "I found a SwiftUI component which lets you use Sign in with Apple to login to Firebase! #SwiftUI"
+                    let tweetUrl = "https://github.com/joehinkle11/Login-with-Apple-Firebase-SwiftUI"
+                    let shareString = "https://twitter.com/intent/tweet?text=\(tweetText)&url=\(tweetUrl)"
+                    let escapedShareString = shareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                    let url = URL(string: escapedShareString)!
+                    UIApplication.shared.open(url)
+                }) {
+                    Text("Share 🐦")
+                }
+            }
+            .navigationBarTitle("Sign in with Apple to Firebase with SwiftUI Demo", displayMode: .inline)
+        }
     }
 }
 
---
+++ LoginWithAppleFirebaseSwiftUI/SignInWithAppleToFirebase/SignInWithAppleToFirebase.swift
@@ -0,0 +1,69 @@
+//
+//  SignInWithAppleToFirebase.swift
+//  LoginWithAppleFirebaseSwiftUI
+//
+//  Created by Joseph Hinkle on 12/15/19.
+//  Copyright © 2019 Joseph Hinkle. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import AuthenticationServices
+
+
+final class SignInWithAppleToFirebase: UIViewRepresentable {
+//    @Environment(\.window) var window: UIWindow?
+//    @State var appleSignInDelegates: SignInWithAppleDelegates! = nil
+    var currentNonce: String? // Unhashed nonce.
+    
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return ASAuthorizationAppleIDButton(type: .default, style: .whiteOutline )
+    }
+  
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
+        
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+//    private func randomNonceString(length: Int = 32) -> String {
+//        precondition(length > 0)
+//        let charset: Array<Character> =
+//        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+//        var result = ""
+//        var remainingLength = length
+//
+//        while remainingLength > 0 {
+//            let randoms: [UInt8] = (0 ..< 16).map { _ in
+//                var random: UInt8 = 0
+//                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+//                if errorCode != errSecSuccess {
+//                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+//                }
+//                return random
+//            }
+//
+//            randoms.forEach { random in
+//                if length == 0 {
+//                    return
+//                }
+//
+//                if random < charset.count {
+//                    result.append(charset[Int(random)])
+//                    remainingLength -= 1
+//                }
+//            }
+//        }
+//
+//        return result
+//    }
+//
+//    private func sha256(_ input: String) -> String {
+//        let inputData = Data(input.utf8)
+//        let hashedData = SHA256.hash(data: inputData)
+//        let hashString = hashedData.compactMap {
+//        return String(format: "%02x", $0)
+//        }.joined()
+//
+//        return hashString
+//    }
+}
