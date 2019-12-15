1706f72a6f23af547055803ac64eb689194ec577
Joe Hinkle <joehinkle11@gmail.com>
2019-12-15 13:21:10 -0500

    Initial Commit

---
+++ LoginWithAppleFirebaseSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginWithAppleFirebaseSwiftUI
+//
+//  Created by Joseph Hinkle on 12/15/19.
+//  Copyright © 2019 Joseph Hinkle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
