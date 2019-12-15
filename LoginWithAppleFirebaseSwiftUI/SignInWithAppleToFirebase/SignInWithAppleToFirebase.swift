87d329cfca324c226a33b96c7b017984591138cc
Joe Hinkle <joehinkle11@gmail.com>
2019-12-15 15:30:17 -0500

    some firebase code

--- LoginWithAppleFirebaseSwiftUI/SignInWithAppleToFirebase/SignInWithAppleToFirebase.swift
+++ LoginWithAppleFirebaseSwiftUI/SignInWithAppleToFirebase/SignInWithAppleToFirebase.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SwiftUI
 import AuthenticationServices
+import CryptoKit
 
 
 final class SignInWithAppleToFirebase: UIViewRepresentable {
@@ -17,7 +18,8 @@ final class SignInWithAppleToFirebase: UIViewRepresentable {
     var currentNonce: String? // Unhashed nonce.
     
     func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
-        return ASAuthorizationAppleIDButton(type: .default, style: .whiteOutline )
+        let button = ASAuthorizationAppleIDButton(type: .default, style: .whiteOutline )
+        return button
     }
   
     func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
@@ -25,45 +27,45 @@ final class SignInWithAppleToFirebase: UIViewRepresentable {
     }
     
     // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-//    private func randomNonceString(length: Int = 32) -> String {
-//        precondition(length > 0)
-//        let charset: Array<Character> =
-//        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-//        var result = ""
-//        var remainingLength = length
-//
-//        while remainingLength > 0 {
-//            let randoms: [UInt8] = (0 ..< 16).map { _ in
-//                var random: UInt8 = 0
-//                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-//                if errorCode != errSecSuccess {
-//                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-//                }
-//                return random
-//            }
-//
-//            randoms.forEach { random in
-//                if length == 0 {
-//                    return
-//                }
-//
-//                if random < charset.count {
-//                    result.append(charset[Int(random)])
-//                    remainingLength -= 1
-//                }
-//            }
-//        }
-//
-//        return result
-//    }
-//
-//    private func sha256(_ input: String) -> String {
-//        let inputData = Data(input.utf8)
-//        let hashedData = SHA256.hash(data: inputData)
-//        let hashString = hashedData.compactMap {
-//        return String(format: "%02x", $0)
-//        }.joined()
-//
-//        return hashString
-//    }
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if length == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
 }
